dccbd18925fee633a155c69ea66fdc431185777b
Federico Trani <federicotrani@gmail.com>
2024-02-21 17:15:34 -0300

    Agregar archivos de proyecto.

---
+++ MauiMicrochartsSample/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microcharts;
+using SkiaSharp;
+
+namespace MauiMicrochartsSample;
+public partial class MainPage : ContentPage
+{
+    ChartEntry[] entries = new[]
+    {
+        new ChartEntry(212)
+        {
+            Label="Windows",
+            ValueLabel="112",
+            Color=SKColor.Parse("#2c3e50")
+        },
+        new ChartEntry(248)
+        {
+            Label="Android",
+            ValueLabel="648",
+            Color=SKColor.Parse("#77d065")
+        },
+        new ChartEntry(128)
+        {
+            Label="IOS",
+            ValueLabel="428",
+            Color=SKColor.Parse("#b455b6")
+        },
+        new ChartEntry(514)
+        {
+            Label=".NET MAUI",
+            ValueLabel="214",
+            Color=SKColor.Parse("#3498db")
+        },
+    };
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        chartView.Chart = new BarChart
+        {
+            Entries = entries
+        };
+
+        chartView1.Chart = new DonutChart
+        {
+            Entries = entries
+        };
+    }
+
+    
+}
